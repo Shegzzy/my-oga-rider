a43536b03bdb6854b330276ba188b8a31551a154
Segun <miksegun1996@gmail.com>
2023-10-29 20:01:21 +0100

    added date formatter

--- lib/utils/formatter/formatter.dart
+++ lib/utils/formatter/formatter.dart
@@ -1,6 +1,10 @@
 import 'package:intl/intl.dart';
 
 class MyOgaFormatter{
+  static String dateFormatter(DateTime? date){
+    date ??= DateTime.now();
+    return DateFormat('dd/MM/yyyy').format(date);
+  }
 
   static String currencyFormatter(double amount){
     return NumberFormat.currency(locale: 'en_NGN', symbol: '₦ ', decimalDigits: 0).format(amount);
